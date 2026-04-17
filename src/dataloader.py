580dd8b49cb9cbf1937630dcc3565e0662c4e54f
linbenji <lin.benja@northeastern.edu>
2026-04-16 20:14:21 -0400

    pushing results, minor update to dataloader and added .gitignore

--- src/dataloader.py
+++ src/dataloader.py
@@ -10,7 +10,7 @@ from src.augment import XBDDatasetAugmented
 
 def get_loaders(train_files, val_files, test_files,
     img_dir, mask_dir,
-    batch_size=4, num_workers=0, augment_train=False):
+    batch_size=4, num_workers=4, pin_memory = True, augment_train=False):
 
 
     train_dataset = XBDDatasetAugmented(train_files, img_dir, mask_dir, augment=augment_train)
@@ -18,10 +18,10 @@ def get_loaders(train_files, val_files, test_files,
     test_dataset = XBDDatasetAugmented(test_files, img_dir, mask_dir, augment=augment_train)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size,
-                              shuffle=True, num_workers = num_workers)
+                              shuffle=True, num_workers = num_workers, pin_memory = pin_memory)
     val_loader = DataLoader(val_dataset, batch_size=batch_size,
-                            shuffle=False, num_workers = num_workers)
+                            shuffle=False, num_workers = num_workers, pin_memory = pin_memory)
     test_loader = DataLoader(test_dataset, batch_size=batch_size,
-                             shuffle=False, num_workers = num_workers)
+                             shuffle=False, num_workers = num_workers, pin_memory = pin_memory)
 
     return train_loader, val_loader, test_loader
